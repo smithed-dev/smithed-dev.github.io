ecba499b4ee3049cb995f6d8c909f03bc3ab23d8
BubbleFish <daforsastudia@gmail.com>
2025-03-29 18:27:19 +0300

    feat: Add a functioning dropdown

---
+++ docs/assets/script.js
@@ -0,0 +1,22 @@
+/** @param {HTMLButtonElement} node */
+function openDropdown(node) {
+  if (node.parentElement.classList.contains("open")) {
+    node.parentElement.classList.remove("open");
+  } else {
+    node.parentElement.classList.add("open");
+  }
+}
+
+const DROPDOWNS = document.querySelectorAll(".w-dropdown");
+
+window.addEventListener(
+  "click",
+  /** @param {PointerEvent} event */
+  function (event) {
+    DROPDOWNS.forEach((dropdown) => {
+      if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove("open");
+      }
+    });
+  },
+);
